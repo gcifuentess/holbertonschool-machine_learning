168e98a3a55bfb6e00d556041940a6d9bfcc720e
Gabriel Cifuentes <1795@holbertonschool.com>
2021-04-15 19:35:33 +0000

    add 7-gettin_cozy.py 7-main.py

---
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+'''module to concatenate two matrices along a specific axis'''
+
+
+def cat_matrices2D(mat1, mat2, axis=0):
+    '''concatenates two matrices along a specific axis'''
+    concat = []
+    for row in mat1:
+        concat.append(row.copy())
+    if axis == 0:
+        for row in mat2:
+            concat.append(row.copy())
+    elif axis == 1:
+        for i in range(0, len(mat1)):
+            for j in range(0, len(mat2[i])):
+                concat[i].append(mat2[i][j])
+    else:
+        concat = None
+    return (concat)
