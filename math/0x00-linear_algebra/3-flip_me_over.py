4be6cea0c859402a8d3b5cabe99b070606896faf
Gabriel Cifuentes <1795@holbertonschool.com>
2021-04-15 00:44:31 +0000

    add 3-flip_me_over.py 3-main.py

---
+++ math/0x00-linear_algebra/3-flip_me_over.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+
+def matrix_transpose(matrix):
+    '''returns the transpose of a 2D matrix'''
+    transpose = []
+    for i in range(0, len(matrix[0])):  # loop in columns
+        new_row = []
+        for row in matrix:  # loop in rows
+            new_row.append(row[i])
+        transpose.append(new_row)
+    return (transpose)
