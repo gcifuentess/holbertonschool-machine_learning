049a57da50ab192b0bc4cc0ab75e906ddc635f2a
Gabriel Cifuentes <1795@holbertonschool.com>
2021-04-15 19:45:07 +0000

    fixed 7-gettin_cozy.py - added conditionals

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -7,10 +7,10 @@ def cat_matrices2D(mat1, mat2, axis=0):
     concat = []
     for row in mat1:
         concat.append(row.copy())
-    if axis == 0:
+    if axis == 0 and len(mat1[0]) == len(mat2[0]):
         for row in mat2:
             concat.append(row.copy())
-    elif axis == 1:
+    elif axis == 1 and len(mat1) == len(mat2):
         for i in range(0, len(mat1)):
             for j in range(0, len(mat2[i])):
                 concat[i].append(mat2[i][j])
