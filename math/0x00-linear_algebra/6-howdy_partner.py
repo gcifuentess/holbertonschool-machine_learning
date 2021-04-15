18a948218da70110b2826952b20c3711646d161a
Gabriel Cifuentes <1795@holbertonschool.com>
2021-04-15 02:20:11 +0000

    add 6-howdy_partner.py 6-main.py

---
+++ math/0x00-linear_algebra/6-howdy_partner.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''module that concatenates two arrays'''
+
+
+def cat_arrays(arr1, arr2):
+    '''concatenates two arrays'''
+    concat = arr1.copy()
+    for element in arr2:
+        concat.append(element)
+    return concat
