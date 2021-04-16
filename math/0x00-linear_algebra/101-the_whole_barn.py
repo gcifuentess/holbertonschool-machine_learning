9387227da17e3dd22b716aaa314a9cdbf295c514
Gabriel Cifuentes <1795@holbertonschool.com>
2021-04-16 02:11:53 +0000

    add 101-the_whole_barn.py 101-main.py

--- math/0x00-linear_algebra/101-the_whole_barn.py
+++ math/0x00-linear_algebra/101-the_whole_barn.py
@@ -1,13 +1,36 @@
 #!/usr/bin/env python3
 '''module that adds two matrices'''
-import numpy as np
 
 
 def add_matrices(mat1, mat2):
     '''adds two matrices'''
-    np_mat1 = np.array(mat1)
-    np_mat2 = np.array(mat2)
-    if np_mat1.shape != np_mat2.shape:
+    shape1 = matrix_shape(mat1)
+    shape2 = matrix_shape(mat2)
+    if shape1 != shape2:
         return None
     else:
-        return ((np_mat1 + np_mat2).tolist())
+        return in_dimension(mat1, mat2, len(shape1), shape1[0])
+
+
+def matrix_shape(matrix):
+    '''calculates the shape of a matrix'''
+    shape = []
+    dimension = matrix.copy()
+    while type(dimension) == list:
+        shape.append(len(dimension))
+        dimension = dimension[0]
+    return(shape)
+
+
+def in_dimension(mat1, mat2, dim, size):
+    '''recursive function'''
+    arr = []
+    if dim == 1:
+        for i in range(0, size):
+            arr.append(mat1[i] + mat2[i])
+        return arr
+    else:
+        for i in range(0, size):
+            to_size = matrix_shape(mat1[i])[0]
+            arr.append(in_dimension(mat1[i], mat2[i], dim - 1, to_size))
+    return arr
