c055cb89a29e541fa9812cb11dd8b7741e32e668
Gabriel Cifuentes <1795@holbertonschool.com>
2021-04-22 23:14:13 +0000

    fix 17-integrate.py - bug fix

--- math/0x02-calculus/17-integrate.py
+++ math/0x02-calculus/17-integrate.py
@@ -6,8 +6,6 @@ def poly_integral(poly, C=0):
     '''calculates the integral of a polynomial'''
     if type(poly) != list or len(poly) == 0 or type(C) != int:
         return None
-    if len(poly) == 1:
-        return [C]
     integral = [C]
     for i in range(0, len(poly)):
         if type(poly[i]) != int and type(poly[i]) != float:
