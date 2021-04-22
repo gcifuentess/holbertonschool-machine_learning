a60313b10fd3a80213da97eb10e59390384ce3b5
Gabriel Cifuentes <1795@holbertonschool.com>
2021-04-22 22:47:36 +0000

    add 17-integrate.py 17-main.py

---
+++ math/0x02-calculus/17-integrate.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+'''module that calculates the integral of a polynomial'''
+
+
+def poly_integral(poly, C=0):
+    '''calculates the integral of a polynomial'''
+    if type(poly) != list or len(poly) == 0 or type(C) != int:
+        return None
+    if len(poly) == 1:
+        return [C]
+    integral = [C]
+    for i in range(0, len(poly)):
+        coeficient = poly[i] / (i + 1)
+        if coeficient - int(coeficient) == 0:
+            coeficient = int(coeficient)
+        integral.append(coeficient)
+    return integral
