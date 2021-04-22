45abc9620d47b390b6c57f4297eefaa81ca9ff71
Gabriel Cifuentes <1795@holbertonschool.com>
2021-04-22 23:30:49 +0000

    fix 10-matisse.py - return None if invalid polynomial

--- math/0x02-calculus/10-matisse.py
+++ math/0x02-calculus/10-matisse.py
@@ -5,7 +5,7 @@
 def poly_derivative(poly):
     '''calculates the derivative of a polynomial'''
     if type(poly) != list or len(poly) == 0:
-        return [0]
+        return None
     derivative = []
     for i in range(1, len(poly)):
         derivative.append(i * poly[i])
