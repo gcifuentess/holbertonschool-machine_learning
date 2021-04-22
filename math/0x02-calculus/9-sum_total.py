9e837c63eb3ea7d158521c209e13512a21325b47
Gabriel Cifuentes <1795@holbertonschool.com>
2021-04-22 23:39:10 +0000

    fix 9-sum_total.py - total refactor, can't use Numpy

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 '''summation_i_squared module'''
-import numpy as np
 
 
 def summation_i_squared(n):
     '''sumatory of i squared'''
-    if (type(n) != int and type(n) != float) or n < 1:
+    if (type(n) != int and type(n) != float) or n == 0:
         return None
-    sequence = np.array(range(1, n + 1)) ** 2
-    return int(sum(sequence, 0))
+    if n == 1:
+        return 1
+    return (n ** 2 + summation_i_squared(n - 1))
