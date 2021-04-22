42d96c203920e877522a48d2a47c04279cb46178
Gabriel Cifuentes <1795@holbertonschool.com>
2021-04-22 23:52:33 +0000

    fix 9-sum_total.py - bugg

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -6,4 +6,4 @@ def summation_i_squared(n):
     '''sumatory of i squared'''
     if (type(n) != int and type(n) != float) or n == 0:
         return None
-    return sum(range(1, (n * n) + 1, n))
+    return int(sum(range(1, (n * n) + 1, n)))
