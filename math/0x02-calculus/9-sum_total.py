cc5f126abd36f10dcb91093df2ff8120d39ce2c3
Gabriel Cifuentes <1795@holbertonschool.com>
2021-04-23 00:01:56 +0000

    fix 9-sum_total.py - total refactor, must use Faulhaber's formula: (Sums of Powers)

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -4,7 +4,7 @@
 
 def summation_i_squared(n):
     '''sumatory of i squared'''
-    if (type(n) != int and type(n) != float) or n == 0:
+    if (type(n) != int and type(n) != float) or n < 1:
         return None
     # Summs of powers Faulhaber's formula:
     return int((n * (n + 1) * (2 * n + 1)) / 6)
