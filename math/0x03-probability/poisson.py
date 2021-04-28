80f60e4b5885f1dec910568056578fc3bbe5b7bb
Gabriel Cifuentes <1795@holbertonschool.com>
2021-04-28 18:27:37 +0000

    update poisson.py - cdf added; add 2-main.py

--- math/0x03-probability/poisson.py
+++ math/0x03-probability/poisson.py
@@ -51,6 +51,8 @@ class Poisson():
         '''Calculates the value of the PMF for a given number of successes
         Args:
             k: is the number of “successes”. if out of range, return 0
+
+        Returns: PMF value for k
         '''
         e = 2.7182818285
         if type(k) is not int:
@@ -61,3 +63,36 @@ class Poisson():
         for i in reversed(range(1, k)):
             x *= i
         return ((e ** -self.lambtha) * (self.lambtha ** k)) / x
+
+    def cdf(self, k):
+        '''Calculates the value of the CDF for a given number of successes
+        Args:
+            k: is the number of “successes”. if out of range, return 0
+        Returns: CDF value for k
+        '''
+        e = 2.7182818285
+        if type(k) is not int:
+            k = int(k)
+        if k < 0:
+            return 0
+
+        z = 0
+        for i in range(floor(k) + 1):
+            x = i
+            if i == 0:
+                x = 1
+            else:
+                for j in reversed(range(1, i)):
+                    x *= j
+            z += (self.lambtha ** i) / x
+        return (e ** -self.lambtha) * z
+
+
+def floor(number):
+    '''Gives as output the greatest integer less than or equal to number
+    Args:
+        number: any Real number
+    Returns: the floor number
+             -For example,floor(2.4) = 2
+    '''
+    return int(number)
