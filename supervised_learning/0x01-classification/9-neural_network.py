8b7ced0399c3d69bd41e5020120dc0b0797512d3
Gabriel Cifuentes <1795@holbertonschool.com>
2021-05-07 20:51:35 +0000

    add 9-neural_network.py 9-main.py

---
+++ supervised_learning/0x01-classification/9-neural_network.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+'''Neural Network module'''
+import numpy as np
+
+
+class NeuralNetwork():
+    '''defines a neural network with one hidden layer performing binary
+    classification'''
+
+    def __init__(self, nx, nodes):
+        '''Neuron Network constructor
+        Args:
+            nx: is the number of input features to the neuron
+            nodes: is the number of nodes found in the hidden layer
+        '''
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if type(nodes) is not int:
+            raise TypeError("nodes must be an integer")
+        if nodes < 1:
+            raise ValueError("nodes must be a positive integer")
+
+        self.__W1 = np.random.randn(nodes, nx)
+        self.__b1 = np.zeros((nodes, 1))
+        self.__A1 = 0
+        self.__W2 = np.random.randn(1, nodes)
+        self.__b2 = 0
+        self.__A2 = 0
+
+    @property
+    def W1(self):
+        '''W1 getter'''
+        return self.__W1
+
+    @property
+    def b1(self):
+        '''b1 getter'''
+        return self.__b1
+
+    @property
+    def A1(self):
+        '''A1 getter'''
+        return self.__A1
+
+    @property
+    def W2(self):
+        '''W2 getter'''
+        return self.__W2
+
+    @property
+    def b2(self):
+        '''b2 getter'''
+        return self.__b2
+
+    @property
+    def A2(self):
+        '''A2 getter'''
+        return self.__A2
