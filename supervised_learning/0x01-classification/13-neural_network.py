fix 13-neural_network.py - b2 was been afectected by gradient decent before its time

supervised_learning/0x01-classification/13-neural_network.py
@@ -127,10 +127,10 @@ class NeuralNetwork():
         dZ2 = A2 - Y  # shape (1, m)
         dW2 = np.matmul(dZ2, A1.T) / m  # shape (1, nodes)
         db2 = (1 / m) * np.sum(dZ2, axis=1, keepdims=True)
-        self.__W2 = self.W2 - alpha * dW2
-        self.__b2 = self.b2 - alpha * db2
-        dZ1 = np.matmul(self.W2.T, dZ2) * (A1 * (1 - A1))  # shape (nodes, m)
+        dZ1 = np.matmul((self.W2).T, dZ2) * (A1 * (1 - A1))  # shape (nodes, m)
         dW1 = (1 / m) * (np.matmul(dZ1, X.T))
-        db1 = (1 / m) * np.sum(dZ1, axis=1, keepdims=True)
+        db1 = np.sum(dZ1, axis=1, keepdims=True) / m  # shape (nodes, 1)
         self.__W1 = self.W1 - alpha * dW1
         self.__b1 = self.b1 - alpha * db1
+        self.__W2 = self.W2 - alpha * dW2
+        self.__b2 = self.b2 - alpha * db2