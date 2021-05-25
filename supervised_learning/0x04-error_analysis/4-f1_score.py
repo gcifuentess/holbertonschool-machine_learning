440eadfdf76642c6c485ba9a746d3091660acd5c
Gabriel Cifuentes <1795@holbertonschool.com>
2021-05-25 01:24:30 +0000

    add 4-f1_score.py 4-main.py

---
+++ supervised_learning/0x04-error_analysis/4-f1_score.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+'''F1 score of a confusion matrix module'''
+import numpy as np
+sensitivity = __import__('1-sensitivity').sensitivity
+precision = __import__('2-precision').precision
+
+
+def f1_score(confusion):
+    '''calculates the F1 score of a confusion matrix
+    Args:
+        confusion is a confusion numpy.ndarray of shape (classes, classes)
+                  where row indices represent the correct labels and column
+                  indices represent the predicted labels
+        classes is the number of classes
+    Returns: a numpy.ndarray of shape (classes,) containing the F1 score of
+             each class
+    '''
+    recall = sensitivity(confusion)
+    _precision = precision(confusion)
+    return 2 * (_precision * recall / (_precision + recall))
