df8569d8a97c694efec375e4599545114c4da81d
Gabriel Cifuentes <1795@holbertonschool.com>
2021-06-01 16:08:22 +0000

    add 8-train.py 8-main.py

---
+++ supervised_learning/0x06-keras/8-train.py
@@ -0,0 +1,72 @@
+'''Save Only the Best module'''
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size, epochs,
+                validation_data=None, early_stopping=False,
+                patience=0, learning_rate_decay=False, alpha=0.1,
+                decay_rate=1, save_best=False, filepath=None,
+                verbose=True, shuffle=False):
+    '''trains a model using mini-batch gradient descent
+    Args:
+        network is the model to train
+        data is a numpy.ndarray of shape (m, nx) containing the input data
+        labels is a one-hot numpy.ndarray of shape (m, classes) containing
+               the labels of data
+        batch_size is the size of the batch used for mini-batch gradient
+                   descent
+        epochs is the number of passes through data for mini-batch gradient
+               descent
+        validation_data is the data to validate the model with, if not None
+        early_stopping is a boolean that indicates whether early stopping
+                       should be used
+                       - only performed if validation_data exists
+                       - based on validation loss
+        patience is the patience used for early stopping
+        learning_rate_decay is a boolean that indicates whether learning rate
+                            decay should be used
+                            - only performed if validation_data exists
+                            - performed using inverse time decay
+                            - decays in a stepwise fashion after each epoch
+                            - each time the learning rate updates, Keras
+                              prints a message
+        alpha is the initial learning rate
+        decay_rate is the decay rate
+        save_best is a boolean indicating whether to save the model after each
+                  epoch if it is the best
+                  - a model is considered the best if its validation loss is
+                    the lowest that the model has obtained
+        filepath is the file path where the model should be saved
+        verbose is a boolean that determines if output should be printed
+                during training
+        shuffle is a boolean that determines whether to shuffle the batches
+                every epoch. Normally, it is a good idea to shuffle, but for
+                reproducibility, we have chosen to set the default to False.
+    Returns: the History object generated after training the model
+    '''
+    callbacks = []
+    if validation_data:
+        if early_stopping:
+            early_stop = K.callbacks.EarlyStopping(monitor='val_loss',
+                                                   patience=patience)
+            callbacks.append(early_stop)
+        if learning_rate_decay:
+            def schedule(step):
+                '''stepwise inverse time decay function'''
+                return alpha * 1 / (1 + decay_rate * step)
+            lr_decay = K.callbacks.LearningRateScheduler(schedule=schedule,
+                                                         verbose=1)
+            callbacks.append(lr_decay)
+        if save_best:
+            save_b = K.callbacks.ModelCheckpoint(filepath=filepath,
+                                                 monitor='val_loss',
+                                                 save_best_only=True)
+            callbacks.append(save_b)
+    return network.fit(x=data,
+                       y=labels,
+                       batch_size=batch_size,
+                       validation_data=validation_data,
+                       epochs=epochs,
+                       verbose=verbose,
+                       callbacks=callbacks,
+                       shuffle=shuffle)
