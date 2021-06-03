208b0de07c91de53a63662181252a0512f590db7
Gabriel Cifuentes <1795@holbertonschool.com>
2021-06-03 18:17:17 +0000

    add 1-convolve_grayscale_same.py 1-main.py

---
+++ math/0x04-convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+'''Same Convolution module'''
+import numpy as np
+
+
+def convolve_grayscale_same(images, kernel):
+    '''performs a same convolution on grayscale images
+    Args:
+        images is a numpy.ndarray with shape (m, h, w) containing multiple
+               grayscale images
+               - m is the number of images
+               - h is the height in pixels of the images
+               - w is the width in pixels of the images
+        kernel is a numpy.ndarray with shape (kh, kw) containing the kernel
+               for the convolution
+               - kh is the height of the kernel
+               - kw is the width of the kernel
+    Important: if necessary, the image is padded with 0’s
+    Returns: a numpy.ndarray containing the convolved images
+    '''
+    m = images.shape[0]
+    input_h = images.shape[1]
+    input_w = images.shape[2]
+    kernel_h = kernel.shape[0]
+    kernel_w = kernel.shape[1]
+    bias = 0  # constant for now
+    padding_h = (kernel_h - 1) // 2
+    padding_w = (kernel_w - 1) // 2
+    output_h = input_h + (2 * padding_h) - kernel_h + 1
+    output_w = input_w + (2 * padding_w) - kernel_w + 1
+
+    output = np.zeros([m, output_h, output_w])  # convolution output
+    images_padded = np.zeros([m, input_h + 2 * padding_h,
+                             input_w + 2 * padding_w])
+    images_padded[:, padding_h: -padding_h, padding_w:-padding_w] = images
+
+    for h in range(output_h):
+        for w in range(output_w):
+            current = images_padded[:, h: h + kernel_h, w: w + kernel_w]
+            # axes sum-reduction:
+            # https://stackoverflow.com/questions/41870228/...
+            # ...understanding-tensordot
+            output[:, h, w] = np.tensordot(current, kernel,
+                                           axes=([1, 2], [0, 1])) + bias
+    return output
