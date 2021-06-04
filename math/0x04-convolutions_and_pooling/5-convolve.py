64a4bff30181127fbed7d77b808e52d5138a680d
Gabriel Cifuentes <1795@holbertonschool.com>
2021-06-04 00:45:41 +0000

    update 5-convolve.py - for loop removed

--- math/0x04-convolutions_and_pooling/5-convolve.py
+++ math/0x04-convolutions_and_pooling/5-convolve.py
@@ -32,6 +32,7 @@ def convolve(images, kernels, padding='same', stride=(1, 1)):
     '''
     m = images.shape[0]
     channels = images.shape[3]
+    kernel_ch = kernels.shape[2]
     n_kernels = kernels.shape[3]
     input_h = images.shape[1]
     input_w = images.shape[2]
@@ -70,19 +71,18 @@ def convolve(images, kernels, padding='same', stride=(1, 1)):
                                     (pad_left, pad_right), (0, 0)],
                            mode="constant")
 
-    for k in range(n_kernels):
-        h = 0
-        for i in range(output_h):
-            w = 0
-            for j in range(output_w):
-                current = images_padded[:, h: h + kernel_h, w: w + kernel_w]
-                # axes sum-reduction:
-                # https://stackoverflow.com/questions/41870228/...
-                # ...understanding-tensordot
-                output[:, i, j, k] = np.tensordot(current, kernels[..., k],
-                                                  axes=([1, 2, 3],
-                                                        [0, 1, 2])) + bias
-                w += stride_w
-            h += stride_h
+    h = 0
+    for i in range(output_h):
+        w = 0
+        for j in range(output_w):
+            current = images_padded[:, h: h + kernel_h, w: w + kernel_w, :]
+            # axes sum-reduction:
+            # https://stackoverflow.com/questions/41870228/...
+            # ...understanding-tensordot
+            output[:, i, j, :] = np.tensordot(current, kernels,
+                                              axes=([1, 2, 3],
+                                                    [0, 1, 2])) + bias
+            w += stride_w
+        h += stride_h
 
     return output
