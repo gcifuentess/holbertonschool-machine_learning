f7dd359d049325cd73b7fa495ce09f4525fddc54
Gabriel Cifuentes <1795@holbertonschool.com>
2021-06-14 05:06:59 +0000

    fix 3-pool_backward.py - max pooling fixed

--- supervised_learning/0x07-cnn/3-pool_backward.py
+++ supervised_learning/0x07-cnn/3-pool_backward.py
@@ -30,23 +30,24 @@ def pool_backward(dA, A_prev, kernel_shape, stride=(1, 1), mode='max'):
      Returns: the partial derivatives with respect to the previous layer
               (dA_prev)
     '''
-    m, h_new, w_new, c_new = dA.shape
+    m, dAh, dAw, ch = dA.shape
     kh, kw = kernel_shape
     sh, sw = stride
 
     dA_prev = np.zeros(A_prev.shape)
 
-    for i in range(m):
-        for h in range(h_new):
-            for w in range(w_new):
-                for c in range(c_new):
-                    s_h = h * sh
-                    s_w = w * sw
-                    box = dA[i, h, w, c]
-                    if mode == 'max':
-                        tmp = A_prev[i, s_h:kh+s_h, s_w:kw+s_w, c]
-                        mask = (tmp == np.max(tmp))
-                        dA_prev[i, s_h:kh+s_h, s_w:kw+s_w, c] += box * mask
-                    if mode == 'avg':
-                        dA_prev[i, s_h:kh+s_h, s_w:kw+s_w, c] += box/kh/kw
+    for m_ in range(m):
+        for i in range(dAh):
+            for j in range(dAw):
+                for ch_ in range(ch):
+                    h = i * sh
+                    w = j * sw
+                    grad = dA[m_, i, j, ch_]
+
+                    if mode == "max":
+                        current = A_prev[i, h: h + kh, w: w + kw, ch_]
+                        kernel = (current == np.max(current))
+                        dA_prev[m_, h: h + kh, w: w + kw, ch_] += grad * kernel
+                    if mode == "avg":
+                        dA_prev[m_, h: h + kh, w: w + kw, c] += grad / kh / kw
     return dA_prev
