d7a62b8d1244029d2d1f0150e896ce61116f28bd
Gabriel Cifuentes <1795@holbertonschool.com>
2021-06-17 14:13:15 +0000

    add 2-identity_block.py 2-main.py

---
+++ supervised_learning/0x08-deep_cnns/2-identity_block.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+'''Identity Block module'''
+import tensorflow.keras as K
+
+
+def identity_block(A_prev, filters):
+    '''Builds an identity block as described in Deep Residual Learning for
+    Image Recognition (2015)
+    Args:
+        A_prev is the output from the previous layer
+        filters is a tuple or list containing F11, F3, F12, respectively:
+                - F11 is the number of filters in the first 1x1 convolution
+                - F3 is the number of filters in the 3x3 convolution
+                - F12 is the number of filters in the second 1x1 convolution
+    Important: All convolutions inside the inception block  use a
+               rectified linear activation (ReLU)
+    Returns: the activated output of the projection block
+    '''
+    F11, F3, F12 = filters
+    w = K.initializers.he_normal(seed=None)
+
+    l1 = K.layers.Conv2D(filters=F11,
+                         kernel_size=(1, 1),
+                         padding="same",
+                         activation="relu",
+                         kernel_initializer=w)(A_prev)
+
+    l2 = K.layers.BatchNormalization()(l1)
+    l2 = K.layers.Activation("relu")(l2)
+
+    l3 = K.layers.Conv2D(filters=F3,
+                         kernel_size=(3, 3),
+                         padding="same",
+                         activation="relu",
+                         kernel_initializer=w)(l2)
+
+    l4 = K.layers.BatchNormalization()(l3)
+    l4 = K.layers.Activation("relu")(l4)
+
+    l5 = K.layers.Conv2D(filters=F12,
+                         kernel_size=(1, 1),
+                         padding="same",
+                         activation="relu",
+                         kernel_initializer=w)(l4)
+
+    l6 = K.layers.BatchNormalization()(l5)
+
+    l7 = K.layers.Add()([l6, A_prev])
+
+    return K.layers.Activation("relu")(l7)
