15c56b2cf0198dc203a04622739f4792f1c18d68
Gabriel Cifuentes <1795@holbertonschool.com>
2021-07-08 20:41:09 +0000

    fix 5-neural_style.py - better weight ponderation

--- supervised_learning/0x0C-neural_style_transfer/5-neural_style.py
+++ supervised_learning/0x0C-neural_style_transfer/5-neural_style.py
@@ -211,7 +211,7 @@ class NST():
             raise TypeError("style_outputs must be a list with a "
                             "length of {}".format(len_style_layers))
 
-        weight = self.beta / len_style_layers
+        weight = 1 / len_style_layers
         layer_style_costs = []
 
         gram_targets = self.gram_style_features
