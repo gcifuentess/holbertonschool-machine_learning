8b09874b2f8c6c3cd3bb104e375d943e61d7ec9a
Gabriel Cifuentes <1795@holbertonschool.com>
2021-07-08 19:29:54 +0000

    fix 3-neural_style.py - content features shouldn't go through gram_matrix function

--- supervised_learning/0x0C-neural_style_transfer/3-neural_style.py
+++ supervised_learning/0x0C-neural_style_transfer/3-neural_style.py
@@ -164,12 +164,9 @@ class NST():
             self.content_image * 255,
         )
 
-        style_features = self.model(p_style_image)[:-1]
-        content_feature = self.model(p_content_image)[-1]
+        n_style_layers = len(self.style_layers)
+        style_features = self.model(p_style_image)[:n_style_layers]
 
-        gram_style_features = [self.gram_matrix(style_feature)
-                               for style_feature in style_features]
-        content_feature = self.gram_matrix(content_feature)
-
-        self.gram_style_features = gram_style_features
-        self.content_feature = content_feature
+        self.content_feature = self.model(p_content_image)[n_style_layers:][0]
+        self.gram_style_features = [self.gram_matrix(style_feature)
+                                    for style_feature in style_features]
