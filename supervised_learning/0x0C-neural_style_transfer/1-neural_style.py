55a978d5bfc2e56912eee107b02a572da938e83c
Gabriel Cifuentes <1795@holbertonschool.com>
2021-07-08 01:01:00 +0000

    update 1-neural_style.py - load_model() now without return

--- supervised_learning/0x0C-neural_style_transfer/1-neural_style.py
+++ supervised_learning/0x0C-neural_style_transfer/1-neural_style.py
@@ -52,7 +52,7 @@ class NST():
         self.content_image = self.scale_image(content_image)
         self.alpha = alpha
         self.beta = beta
-        self.model = self.load_model()
+        self.load_model()
 
     @staticmethod
     def scale_image(image):
@@ -103,4 +103,4 @@ class NST():
 
         model_outputs = style_outputs + content_outputs
 
-        return (tf.keras.models.Model(vgg.input, model_outputs))
+        self.model = tf.keras.models.Model(vgg.input, model_outputs))
