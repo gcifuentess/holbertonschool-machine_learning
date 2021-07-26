3a0385617e590764b52559112714d2d170dfc91a
Gabriel Cifuentes <1795@holbertonschool.com>
2021-07-26 23:04:37 +0000

    update multinormal.py - now without using any additional function

--- math/0x06-multivariate_prob/multinormal.py
+++ math/0x06-multivariate_prob/multinormal.py
@@ -3,37 +3,6 @@
 import numpy as np
 
 
-def mean_cov(X):
-    '''calculates the mean and covariance of a data set
-    Args:
-        X is a numpy.ndarray of shape (n, d) containing the data set:
-            - n is the number of data points
-            - d is the number of dimensions in each data point
-    Returns: mean, cov:
-        - mean is a numpy.ndarray of shape (1, d) containing the mean of the
-               data set
-        - cov is a numpy.ndarray of shape (d, d) containing the covariance
-              matrix of the data set
-    '''
-    if (type(X) is not np.ndarray or len(X.shape) != 2):
-        raise TypeError("X must be a 2D numpy.ndarray")
-
-    n, d = X.shape
-
-    if (n < 2):
-        raise ValueError("X must contain multiple data points")
-
-    mean = np.mean(X, 0, keepdims=True)
-    std_x = (X - mean)
-    cov = np.matmul(std_x.T, std_x) / (n - 1)
-    # The reason the sample covariance matrix has N-1} the denominator rather
-    # than N is essentially that the population mean E(X) is not known and is
-    # replaced by the sample mean.
-    # From https://en.wikipedia.org/wiki/Covariance
-
-    return mean, cov
-
-
 class MultiNormal():
     '''Represents a Multivariate Normal distribution'''
 
@@ -47,9 +16,11 @@ class MultiNormal():
         if (type(data) is not np.ndarray or len(data.shape) != 2):
             raise TypeError("data must be a 2D numpy.ndarray")
 
-        n, d = data.shape
+        d, n = data.shape
 
         if (n < 2):
             raise ValueError("data must contain multiple data points")
 
-        self.mean, self.cov = mean_cov(data.T)
+        self.mean = np.mean(data, 1, keepdims=True)
+        std_data = (data - self.mean)
+        self.cov = np.matmul(std_data, std_data.T) / (n - 1)
