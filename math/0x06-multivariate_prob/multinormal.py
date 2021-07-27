56e6662ae0afdc15a4c5861a4f29818863a64fd7
Gabriel Cifuentes <1795@holbertonschool.com>
2021-07-27 00:00:26 +0000

    add multinormal.py 3-main.py

--- math/0x06-multivariate_prob/multinormal.py
+++ math/0x06-multivariate_prob/multinormal.py
@@ -24,3 +24,32 @@ class MultiNormal():
         self.mean = np.mean(data, 1, keepdims=True)
         std_data = (data - self.mean)
         self.cov = np.matmul(std_data, std_data.T) / (n - 1)
+        self.n = n
+        self.d = d
+
+    def pdf(self, x):
+        '''calculates the PDF at a data point
+        Args:
+        x is a numpy.ndarray of shape (d, 1) containing the data point
+          whose PDF should be calculated
+            - d is the number of dimensions of the Multinomial instance
+        Returns the value of the PDF
+        '''
+        if (type(x) is not np.ndarray):
+            raise TypeError("x must be a numpy.ndarray")
+
+        d, _ = x.shape
+
+        if (len(x.shape) != 2 or d != self.d):
+            raise ValueError("x must have the shape ({}, 1)".
+                             format(self.d))
+
+        cov_det = np.linalg.det(self.cov)
+
+        const = 1 / (((2 * np.pi) ** (d / 2)) * (cov_det ** (1 / 2)))
+
+        potence = ((x - self.mean).T @
+                   np.linalg.inv(self.cov) @
+                   (x - self.mean)) * (- 1 / 2)
+
+        return np.asscalar(const * np.exp(potence))
