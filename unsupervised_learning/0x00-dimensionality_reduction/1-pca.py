300fa45360474399efdb3c8c4723350fb5b5750e
Gabriel Cifuentes <1795@holbertonschool.com>
2021-07-28 22:37:33 +0000

    add 1-pca.py 1-main.py

---
+++ unsupervised_learning/0x00-dimensionality_reduction/1-pca.py
@@ -0,0 +1,19 @@
+'''Principal Components Analysys (PCA) with SVD, module'''
+import numpy as np
+
+
+def pca(X, ndim):
+    '''performs PCA on a dataset
+    Args:
+        X is a numpy.ndarray of shape (n, d) where:
+            - n is the number of data points
+            - d is the number of dimensions in each point
+        ndim is the new dimensionality of the transformed X
+    Returns: T, a numpy.ndarray of shape (n, ndim) containing the transformed
+             version of X
+    '''
+    std_x = X - np.mean(X, 0)
+    U, S, VH = np.linalg.svd(std_x)
+    W = VH[:ndim]
+
+    return np.matmul(std_x, W.T)
