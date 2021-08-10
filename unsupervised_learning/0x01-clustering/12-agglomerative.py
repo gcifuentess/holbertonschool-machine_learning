daabfa5b8fba54e6086507c794da98b6b7c9a6da
Gabriel Cifuentes <1795@holbertonschool.com>
2021-08-10 15:46:46 +0000

    upgrade 12-agglomerative.py - total upgrade

--- unsupervised_learning/0x01-clustering/12-agglomerative.py
+++ unsupervised_learning/0x01-clustering/12-agglomerative.py
@@ -5,5 +5,22 @@ import matplotlib.pyplot as plt
 
 
 def agglomerative(X, dist):
-    '''performs agglomerative clustering on a dataset'''
-    
+    '''performs agglomerative clustering on a dataset
+    Args:
+        X is a numpy.ndarray of shape (n, d) containing the dataset
+        dist is the maximum cophenetic distance for all clusters
+
+    Important:
+        Performs agglomerative clustering with Ward linkage
+        Displays the dendrogram with each cluster displayed in a different
+            color
+
+    Returns: clss, a numpy.ndarray of shape (n,) containing the cluster indices
+             for each data point
+    '''
+    Z = scipy.cluster.hierarchy.linkage(y=X, method='ward')
+    dendogram = scipy.cluster.hierarchy.dendrogram(Z=Z, color_threshold=dist)
+    clss = scipy.cluster.hierarchy.fcluster(Z=Z, t=dist, criterion='distance')
+    plt.show()  # will call the dendogram
+
+    return clss
