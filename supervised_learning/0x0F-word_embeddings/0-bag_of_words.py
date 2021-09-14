ac985c5a97a2b878d1203d244b83497c6250fb6c
Gabriel Cifuentes <1795@holbertonschool.com>
2021-09-14 19:22:51 +0000

    update 0-bag_of_words.py - now using sklearn

--- supervised_learning/0x0F-word_embeddings/0-bag_of_words.py
+++ supervised_learning/0x0F-word_embeddings/0-bag_of_words.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 '''Bag Of Words module'''
-import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer
 
 
 def bag_of_words(sentences, vocab=None):
@@ -16,47 +16,8 @@ def bag_of_words(sentences, vocab=None):
             - f is the number of features analyzed
         features is a list of the features used for embeddings
     '''
-    loops = 1
-    if vocab is None:
-        loops = 2
+    CV_instance = CountVectorizer(vocabulary=vocab)
+    embeddings = CV_instance.fit_transform(sentences)
+    features = CV_instance.get_feature_names()
 
-    s_vocab = []  # vocabulary from sentences
-    features = []
-
-    # first loop to build the vocabulary if None
-    # second loop to build the embeddings
-    for i in range(loops):
-
-        if vocab is not None:
-            embeddings = np.zeros((len(sentences), len(vocab)), dtype=np.int32)
-
-        for j, sentence in enumerate(sentences):
-            sentence = sentence.lower()
-            words = sentence.split(' ')
-
-            for word in words:
-                p_word = ''  # processed word
-
-                for l in word:
-                    if l.isalpha():  # check for special chars
-                        p_word += l
-                    else:
-                        break
-
-                if vocab is None and p_word not in s_vocab:
-                    s_vocab.append(p_word)
-
-                if vocab is not None:
-                    idx = vocab.index(p_word)
-                    embeddings[j][idx] += 1
-                    if i == 0:
-                        feature = vocab[idx]
-                        if feature not in features:
-                            features.append(feature)
-
-        if vocab is None:
-            s_vocab.sort()
-            vocab = s_vocab.copy()
-            features = s_vocab
-
-    return embeddings, features
+    return embeddings.toarray(), features
