4ff1bea71b724a9a4997dd6fc61fd0ed59d3e887
Gabriel Cifuentes <1795@holbertonschool.com>
2021-09-17 03:03:14 +0000

    add 2-cumulative_bleu.py 2-main.py

---
+++ supervised_learning/0x10-nlp_metrics/2-cumulative_bleu.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+'''Cumulative N-gram BLEU score'''
+import numpy as np
+
+
+def cumulative_bleu(references, sentence, n):
+    '''calculates the cumulative n-gram BLEU score for a sentence
+    Args:
+        references is a list of reference translations
+            - each reference translation is a list of the words in the
+              translation
+        sentence is a list containing the model proposed sentence
+        n is the size of the n-gram to use for evaluation
+
+    Important: All n-gram scores should be weighted evenly
+
+    Returns: the cumulative n-gram BLEU score
+    '''
+    iterations = n
+    Pns = []
+
+    for n in range(1, iterations + 1):
+
+        count = {}
+        count_clip = {}
+        len_refs = []
+
+        sentence_mod = []
+        references_mod = []
+        c = len(sentence)
+
+        for i in range(c - n + 1):
+            n_gram = tuple([sentence[i + j] for j in range(n)])
+            sentence_mod.append(n_gram)
+
+        for reference in references:
+            len_refs.append(len(reference))
+
+            reference_mod = []
+            for i in range(len_refs[-1] - n + 1):
+                n_gram = tuple([reference[i + j] for j in range(n)])
+                reference_mod.append(n_gram)
+
+            references_mod.append(reference_mod)
+
+        for n_gram in set(sentence_mod):
+            count[n_gram] = sentence_mod.count(n_gram)
+
+        for reference_mod in references_mod:
+            for n_gram in set(reference_mod):
+                if n_gram in sentence_mod:
+                    if n_gram in count_clip.keys():
+                        count_clip[n_gram] = max(count_clip[n_gram],
+                                                 reference_mod.count(n_gram))
+                    else:
+                        count_clip[n_gram] = reference_mod.count(n_gram)
+
+        for n_gram in count.keys():
+            if n_gram in count_clip.keys():
+                count_clip[n_gram] = min(count[n_gram], count_clip[n_gram])
+
+        Pn = sum(count_clip.values()) / sum(count.values())
+        Pns.append(Pn)
+
+    Pns = np.array(Pns)
+    len_refs.sort()
+
+    r = len_refs[0]
+    if (c > r):
+        bp = 1  # brevity penalty
+    else:
+        bp = np.exp(1 - r / c)
+
+    return bp * np.exp(np.mean(np.log(Pns)))  # BLUE score
