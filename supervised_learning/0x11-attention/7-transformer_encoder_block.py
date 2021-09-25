d2c95aeac45668ee1a6d8d85d59b0e1bd17398be
Gabriel Cifuentes <1795@holbertonschool.com>
2021-09-25 16:26:11 +0000

    add 7-transformer_encoder_block.py 7-main.py

---
+++ supervised_learning/0x11-attention/7-transformer_encoder_block.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+'''Transformer Encoder Block Module'''
+import tensorflow as tf
+MultiHeadAttention = __import__('6-multihead_attention').MultiHeadAttention
+
+
+class EncoderBlock(tf.keras.layers.Layer):
+    '''creates an encoder block for a transformer'''
+
+    def __init__(self, dm, h, hidden, drop_rate=0.1):
+        '''EncoderBlock class constructor
+        Args:
+            dm - the dimensionality of the model
+            h - the number of heads
+            hidden - the number of hidden units in the fully connected layer
+            drop_rate - the dropout rate
+
+        Sets the following public instance attributes:
+            mha - a MultiHeadAttention layer
+            dense_hidden - the hidden dense layer with hidden units and relu
+                           activation
+            dense_output - the output dense layer with dm units
+            layernorm1 - the first layer norm layer, with epsilon=1e-6
+            layernorm2 - the second layer norm layer, with epsilon=1e-6
+            dropout1 - the first dropout layer
+            dropout2 - the second dropout layer
+        '''
+        super().__init__()
+        self.mha = MultiHeadAttention(dm, h)
+        self.dense_hidden = tf.keras.layers.Dense(hidden, activation='relu')
+        self.dense_output = tf.keras.layers.Dense(dm, activation='relu')
+        self.layernorm1 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
+        self.layernorm2 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
+        self.dropout1 = tf.keras.layers.Dropout(rate=drop_rate)
+        self.dropout2 = tf.keras.layers.Dropout(rate=drop_rate)
+
+    def call(self, x, training, mask=None):
+        '''method to call the instance
+        Args:
+            x - a tensor of shape (batch, input_seq_len, dm) containing the
+                input to the encoder block
+            training - a boolean to determine if the model is training
+            mask - the mask to be applied for multi head attention
+
+        Returns: a tensor of shape (batch, input_seq_len, dm) containing the
+                 block’s output
+        '''
+        Q = self.mha.Wq(x)
+        K = self.mha.Wk(x)
+        V = self.mha.Wv(x)
+
+        smh_attention, _ = self.mha(Q, K, V, mask)
+        skip1 = x + smh_attention  # skip connection
+        norm1 = self.layernorm1(skip1)
+        linear = self.dense_hidden(norm1)
+        linear = self.dropout1(linear)
+        linear = self.dense_output(linear)
+        linear = self.dropout2(linear)
+        skip2 = norm1 + linear  # skip connection
+
+        return self.layernorm2(skip2)
