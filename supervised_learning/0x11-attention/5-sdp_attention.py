82e562157517d573592e0e75b829b2a47d83f9d7
Gabriel Cifuentes <1795@holbertonschool.com>
2021-09-25 00:45:38 +0000

    add 5-sdp_attention.py 5-main.py

---
+++ supervised_learning/0x11-attention/5-sdp_attention.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+'''Scaled Dot Product Attention'''
+import tensorflow as tf
+
+
+def sdp_attention(Q, K, V, mask=None):
+    '''calculates the scaled dot product attention
+    Args:
+        Q is a tensor with its last two dimensions as (..., seq_len_q, dk)
+          containing the query matrix
+        K is a tensor with its last two dimensions as (..., seq_len_v, dk)
+          containing the key matrix
+        V is a tensor with its last two dimensions as (..., seq_len_v, dv)
+          containing the value matrix
+        mask is a tensor that can be broadcast into (..., seq_len_q, seq_len_v)
+             containing the optional mask, or defaulted to None
+            - if mask is not None, multiply -1e9 to the mask and add it to the
+              scaled matrix multiplication
+
+    Important: The preceding dimensions of Q, K, and V are the same
+
+    Returns: output, weights
+        output is a tensor with its last two dimensions as
+               (..., seq_len_q, dv) containing the scaled dot product attention
+        weights is a tensor with its last two dimensions as
+                (..., seq_len_q, seq_len_v) containing the attention weights
+    '''
+    Q_K = tf.matmul(Q, K, transpose_b=True)
+    Q_shape = Q.get_shape().as_list()
+    dk = tf.cast(Q_shape[-1], dtype=tf.float32)
+
+    if mask:
+        Q_K += mask * -1e9
+
+    weights = tf.nn.softmax(Q_K / tf.math.sqrt(dk))
+    attention = tf.matmul(weights, V)
+
+    return attention, weights
