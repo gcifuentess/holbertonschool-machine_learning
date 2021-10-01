02d2ff5180ba1c3800b7d8fc5ccf3a71c072debd
Gabriel Cifuentes <1795@holbertonschool.com>
2021-10-01 00:32:12 +0000

    update 11-transformer.py - the class now inherits from Models

--- supervised_learning/0x11-attention/11-transformer.py
+++ supervised_learning/0x11-attention/11-transformer.py
@@ -5,7 +5,7 @@ Encoder = __import__('9-transformer_encoder').Encoder
 Decoder = __import__('10-transformer_decoder').Decoder
 
 
-class Transformer(tensorflow.keras.Model):
+class Transformer(tf.keras.Model):
     '''creates a transformer network'''
 
     def __init__(self, N, dm, h, hidden, input_vocab, target_vocab,
