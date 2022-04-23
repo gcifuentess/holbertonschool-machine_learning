722ea1fb65f50271d1fb1dc59e08e4cdfcd0c7dd
Gabriel Cifuentes <1795@holbertonschool.com>
2022-04-23 16:50:02 +0000

    add ./1-crop.py

---
+++ pipeline/0x04-data_augmentation/1-crop.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Crop"""
+import tensorflow as tf
+
+
+def crop_image(image, size):
+    """performs a random crop of an image"""
+    return tf.image.random_crop(image, size)
